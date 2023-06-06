9546278e218bc5c88151d27c4cece2d1ab4e8f6d
julianschlaak <100426425+julianschlaak@users.noreply.github.com>
2023-06-06 21:25:54 +0200

    Add files via upload

--- regression_swi_sent1.R
+++ regression_swi_sent1.R
@@ -5,20 +5,25 @@ library(raster)
 library(sf)
 
 rm(list = ls())
-setwd("F:\\loew\\")
+setwd("D:\\Schlaak\\Soilmoisutre_downscaling\\")
 
 #load swi_crop (use the preprocessing_script first)
-swi_data <- data.frame(file = list.files("crop_swi\\", pattern = "*.tif$", recursive = TRUE, full.names = TRUE), stringsAsFactors = FALSE)
-swi_data$date <-substr(swi_data$file, nchar(swi_data$file)-15, nchar(swi_data$file)-8) #23/16 zeichenstellen
-#filter swi files: only SWI data from 2019
-swi_data <- swi_data[grepl("2019", swi_data$file),]
-
+swi_data <- data.frame(file = list.files("crop_swi_40cm\\", pattern = "*.tif$", recursive = TRUE, full.names = TRUE), stringsAsFactors = FALSE)
+swi_data$date <-substr(swi_data$file, nchar(swi_data$file)-15, nchar(swi_data$file)-8) 
 #list all available s1k0 files and extract date information (from file name)
 s1k0_data <- data.frame(file = list.files("crop_sent\\", pattern = glob2rx("*.tif$"), recursive = TRUE, full.names = TRUE), stringsAsFactors = FALSE)
-s1k0_data$date <- substr(s1k0_data$file, nchar(s1k0_data$file)-16, nchar(s1k0_data$file)-9)  # 44 /37 zeichennummern
+s1k0_data$date <- substr(s1k0_data$file, nchar(s1k0_data$file)-19, nchar(s1k0_data$file)-12)  
+#get ndvi
+ndvi_data <- data.frame(file = list.files("crop_ndvi\\", pattern = glob2rx("*.tif$"), recursive = TRUE, full.names = TRUE), stringsAsFactors = FALSE)
+ndvi_data$date <- substr(ndvi_data$file, nchar(ndvi_data$file)-16, nchar(ndvi_data$file)-9)
+#get ndwi
+ndwi_data <- data.frame(file = list.files("crop_ndwi\\", pattern = glob2rx("*.tif$"), recursive = TRUE, full.names = TRUE), stringsAsFactors = FALSE)
+ndwi_data$date <- substr(ndwi_data$file, nchar(ndwi_data$file)-16, nchar(ndwi_data$file)-9)
 
 #merge the file information --> only dates left for which both data is available
 merged_data <- merge(swi_data, s1k0_data, by = "date")
+merged_ndwi<- merge(s1k0_data, ndwi_data, by= "date")
+merged_ndvi<- merge(s1k0_data, ndvi_data, by="date")
 
 #load additional data and convert it to a large dataframe (one row = 1 pixel, each column for each data source)
 #to save RAM: create the dataframe in two steps
@@ -35,20 +40,20 @@ rm(aspect, convergence, elevation, slope) #
 add_data1_vals <- data.frame(getValues(add_data1))
 
 #step 2: second half of the data
-ndvi_mean <- brick("output_preprocessing\\ndvi_mean.tif")
-names(ndvi_mean) <- "ndvi_mean"
-ndvi_sd <- brick("output_preprocessing\\ndvi_std.tif")
-names(ndvi_sd) <- "ndvi_sd"
+#ndvi_mean <- brick("output_preprocessing\\ndvi_mean.tif")
+#names(ndvi_mean) <- "ndvi_mean"
+#ndvi_sd <- brick("output_preprocessing\\ndvi_std.tif")
+#names(ndvi_sd) <- "ndvi_sd"
 #ndwi
-ndwi_mean <- brick("output_preprocessing\\ndwi_mean.tif")
-names(ndwi_mean) <- "nwdi_mean"
-ndwi_sd <- brick("output_preprocessing\\ndwi_std.tif")
-names(ndwi_sd) <- "ndwi_sd"
+#ndwi_mean <- brick("output_preprocessing\\ndwi_mean.tif")
+#names(ndwi_mean) <- "nwdi_mean"
+#ndwi_sd <- brick("output_preprocessing\\ndwi_std.tif")
+#names(ndwi_sd) <- "ndwi_sd"
 #twi
 twi <- brick("output_preprocessing\\twi.tif")
 #stack data 
-add_data2 <- stack(twi, ndvi_mean, ndvi_sd, ndwi_mean, ndwi_sd)
-names(add_data2) <- c("twi", "ndvi_mean", "ndvi_sd", "ndwi_mean", "ndwi_sd")
+add_data2 <- stack(twi)#,ndvi_mean, ndvi_sd, ndwi_mean, ndwi_sd)
+names(add_data2) <- c("twi")#, "ndvi_mean", "ndvi_sd", "ndwi_mean", "ndwi_sd")
 #remove inputs
 rm(twi,ndvi, ndvi_mean, ndvi_sd, ndwi, ndwi_mean, ndwi_sd)
 add_data1_vals <- data.frame(getValues(add_data1))
@@ -59,25 +64,40 @@ add_data_vals <- cbind(add_data1_vals, add_data2_vals)
 rm(add_data1_vals, add_data2_vals)
 
 #vector of all soil depths
-varnames <- c("SWI_001", "SWI_005","SWI_010","SWI_015","SWI_020","SWI_040", "SWI_060", "SWI_100") #ggf. alles unter 15 weg lassen
+varnames <- c("SWI_040") #,"SWI_001", "SWI_005","SWI_015","SWI_020","SWI_040", "SWI_060", "SWI_100") #ggf. alles unter 15 weg lassen
 #create dir if not exists
-out_dir <- "Output"
+out_dir <- "Output_40cm"
 query_dir <- dir.exists(out_dir)
 if(query_dir==F) dir.create(out_dir)
 
 #do the following for all soil depths
 for(v in varnames){
-  #create a data frame where all results are saved (r² and p value of model, p value and coefficient estimate of all variables)
-  model_output <- data.frame(date = merged_data$date, r2_model = NA, p_model = NA,
-                             p_s1 = NA, p_aspect = NA,p_convergence = NA,p_elevation = NA,
-                             p_slope = NA, p_twi = NA, p_ndvi_mean = NA,p_ndvi_sd = NA,
-                             p_ndwi_mean = NA, p_ndwi_sd = NA, beta_s1 = NA,beta_aspect = NA, 
-                             beta_convergence = NA,beta_elevation = NA,beta_slope = NA,
-                             beta_twi = NA, beta_ndvi_mean = NA, beta_ndvi_sd = NA,
-                             beta_ndwi_mean = NA,beta_ndwi_sd = NA)
   
+  #create a data frame where all results are saved (r² and p value of model, p value and coefficient estimate of all variables)
+  model_output <- data.frame(date = merged_data$date,
+                             r2_model = NA,
+                             p_model = NA,
+                             p_s1 = NA,
+                             p_ndvi = NA,
+                             p_ndwi = NA,
+                             p_aspect = NA,
+                             p_convergence = NA,
+                             p_elevation = NA,
+                             p_slope = NA, 
+                             p_twi = NA, 
+                             beta_s1 = NA,
+                             beta_ndvi = NA,
+                             beta_ndwi = NA,
+                             beta_aspect = NA, 
+                             beta_convergence = NA,
+                             beta_elevation = NA,
+                             beta_slope = NA,
+                             beta_twi = NA 
+                             )
+  print(v)
   #for each date, calculate a linear model
   for(i in c(1:nrow(merged_data))){
+    print(merged_data$date[i])
     print(i)
     #load the s1k0 layer of the specific date
     s1 <- brick(merged_data$file.y[i])
@@ -86,9 +106,16 @@ for(v in varnames){
     swi <- brick(merged_data$file.x[i], varname = v)
     #swi <- resample(swi, s1, method = "ngb")
     names(swi) <- "swi"
-    
+    #ndvi
+    ndvi<- brick(merged_ndvi$file.y[i], varnames = v)
+    names(ndvi) <- "ndvi"
+    #
+    ndwi<- brick(merged_ndwi$file.y[i])
+    names(ndvi) <- "ndwi"
+    ndwi <- raster::resample(ndwi , s1, method='bilinear')
+    ndvi <- raster::resample(ndvi , s1, method='bilinear')
     #stack swi and s1k0 layers and extract data to data frame (same as above: 1 row = 1 pixel)
-    data_stack <- stack(swi, s1)
+    data_stack <- stack(swi, s1,ndvi, ndwi)
     data_vals <- data.frame(getValues(data_stack)) #rasterobj
     
     #combine the data with the dataframe with the additional data and remove rows/pixels with missing data (--> cannot be used for linear modelling)
@@ -118,12 +145,14 @@ for(v in varnames){
     residuals_r[] <- residuals
 
     #write the residual raster to a Geotiff (specific date + specific soil depth)
-    writeRaster(residuals_r, paste0("Output\\residuals","_", v, "_", merged_data$date[i],".tif" ), overwrite = TRUE)
-    rm(s1, swi, data_stack, data_vals, model, residuals, residuals_r)
+    writeRaster(residuals_r, paste0("Output_40cm\\residuals","_", v, "_", merged_data$date[i],".tif" ), overwrite = TRUE)
+    print(extent(residuals_r))
+    
+    rm(s1, swi, data_stack, data_vals, model, residuals, residuals_r, ndvi)
     
   }
   #save the model parameters to a file (specific soil depth + all dates)
-  write.csv(model_output, paste0("Output\\residuals", v, ".csv"))
+  write.csv(model_output, paste0("Output_40cm\\residuals", v, ".csv"))
 }
 
 
